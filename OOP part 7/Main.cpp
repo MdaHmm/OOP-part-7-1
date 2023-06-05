1ad7a41c83ecfd8142a2ac6e8f6f4a120dc16a5d
DevC <dimabmwmw@mail.ru>
2023-06-05 19:27:46 +0300

    Добавьте файлы проекта.

---
+++ OOP part 7/Main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+// UTF-8
+
+// Перегрузка операторов
+// linked list
+
+
+//	node - звено
+//	односвязный список
+//	10							-> 12						-> ...
+//	ссылка на следующий объект   -> ссылка на след объект	->
+//	head -----------------------------------------------------> tale
+
+
+class Digit
+{
+private:
+	int _data;
+public:
+
+	Digit(int data) : _data(data) {}
+
+	int operator+(Digit &digit)
+	{
+		return _data + digit._data;
+		//return _data - digit._data + digit._data * digit._data / digit._data;
+	}
+
+	int operator-(Digit& digit)
+	{
+		if (_data < digit._data)
+			return digit._data - _data;
+		return _data - digit._data;
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	
+	Digit digit(5);
+	Digit digit2(10);
+	std::cout << digit.operator+(digit2) << "\n";
+	std::cout << digit.operator-(digit2) << "\n";
+	std::cout << digit - digit2 << "\n";
+	//std::cout << digit + 123 << "\n";
+	
+
+
+	return 0;
+}
